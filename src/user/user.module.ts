db37ac14fcd9e42aef51f7fc86e32a50c219160f
Andrei <coursestud@mail.ru>
2023-12-06 21:37:43 +0300

    feat: add guards for user endpoints

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [
+    UserService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [UserService],
 })
 export class UserModule {}
