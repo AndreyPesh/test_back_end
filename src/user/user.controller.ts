9ca0235e1869b6945426c8b412702dee85498c91
Andrei <coursestud@mail.ru>
2023-12-06 11:27:53 +0300

    feat: add user crud core

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './dto/User.dto';
 
 @Controller('user')
 export class UserController {
@@ -8,5 +17,25 @@ export class UserController {
   @Get('all')
   async getAllUsers() {
     return await this.userService.getAllUsers();
+  }
+
+  @Get(':email')
+  async getUserByEmail(@Param('email') email: string) {
+    return await this.userService.getUserByEmail(email);
+  }
+
+  @Post('create')
+  async createUser(@Body() userData: UserDto) {
+    return await this.userService.createUser(userData);
+  }
+
+  @Patch('update')
+  async updateUser(@Body() userData: UserDto) {
+    return await this.userService.updateUser(userData);
+  }
+
+  @Delete('delete')
+  async deleteUser(@Body() { email }: { email: string }) {
+    return await this.userService.deleteUser(email);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './dto/User.dto';
 
 @Injectable()
 export class UserService {
@@ -8,5 +9,57 @@ export class UserService {
   async getAllUsers() {
     const users = await this.prisma.user.findMany();
     return users;
+  }
+
+  async createUser(user: UserDto) {
+    const { firstName, lastName, email } = user;
+    const newUser = await this.prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        image: '',
+        pdf: Buffer.from([]),
+      },
+    });
+    return newUser;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+
+  async updateUser(userData: UserDto) {
+    const { firstName, lastName, email } = userData;
+    const user = this.prisma.user.upsert({
+      where: {
+        email,
+      },
+      update: {
+        firstName,
+        lastName,
+      },
+      create: {
+        email,
+        firstName,
+        lastName,
+        image: '',
+        pdf: Buffer.from([]),
+      },
+    });
+    return user;
+  }
+
+  async deleteUser(email: string) {
+    return await this.prisma.user.delete({
+      where: {
+        email,
+      },
+    });
   }
 }
